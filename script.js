97ffdc1d37816623e02a0a14cb3b34385bf68a05
Rajeeradha <127470577+Rajeeradha@users.noreply.github.com>
2023-07-10 05:45:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let outputdisplay=document.getElementById("result");
+
+function Display(num){
+    if(isNaN(num)){
+        alert("invalidinput! Please enter a number")
+    }
+    else{
+    outputdisplay.value += num;
+    }
+}
+
+function display(num){
+
+    outputdisplay.value += num;
+}
+
+function Clear(){
+    outputdisplay.value='';
+}
+
+function Delete(){
+    outputdisplay.value=outputdisplay.value.slice(0,-1);
+}
+
+function calculate(){
+    if(outputdisplay.value.length>=1)
+    {
+     try {
+        outputdisplay.value = eval(outputdisplay.value);
+     } catch (error) {
+        alert("invalid value");
+     }}
+    else{
+        alert("enter value")
+     }    
+}
